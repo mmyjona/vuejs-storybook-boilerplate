dfb1fc87a5f1e0953afa4c46e568aaf22829cb88
gaosheng <jonathan.gonse@gmail.com>
2017-08-05 00:27:41 +0800

    add options

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,6 +8,62 @@ import MyButton from '../src/stories/Button.vue'
 Vue.component('my-button', MyButton)
 Vue.use(Vuex)
 
+import { setOptions } from '@storybook/addon-options';
+
+// Option defaults:
+setOptions({
+  /**
+   * name to display in the top left corner
+   * @type {String}
+   */
+  name: 'mmy\'s vue storybook',
+  /**
+   * URL for name in top left corner to link to
+   * @type {String}
+   */
+  url: '#',
+  /**
+   * show story component as full screen
+   * @type {Boolean}
+   */
+  goFullScreen: false,
+  /**
+   * display left panel that shows a list of stories
+   * @type {Boolean}
+   */
+  showLeftPanel: true,
+  /**
+   * display horizontal panel that displays addon configurations
+   * @type {Boolean}
+   */
+  showDownPanel: true,
+  /**
+   * display floating search box to search through stories
+   * @type {Boolean}
+   */
+  showSearchBox: false,
+  /**
+   * show horizontal addons panel as a vertical panel on the right
+   * @type {Boolean}
+   */
+  downPanelInRight: false,
+  /**
+   * sorts stories
+   * @type {Boolean}
+   */
+  sortStoriesByKind: false,
+  /**
+   * regex for finding the hierarchy separator
+   * @example:
+   *   null - turn off hierarchy
+   *   /\// - split by `/`
+   *   /\./ - split by `.`
+   *   /\/|\./ - split by `/` or `.`
+   * @type {Regex}
+   */
+  hierarchySeparator: '/\/|\./',
+});
+
 function loadStories() {
   require('../src/stories');
 }
